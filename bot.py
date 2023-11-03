72eafe91a8e4cb8dbb42b23969967f72ad8a1a05
ParkJinho <pen40@naver.com>
2023-11-03 18:55:28 +0900

    delete current time & not available code

--- bot.py
+++ bot.py
@@ -3,93 +3,51 @@
 # https://learn.microsoft.com/ko-kr/azure/ai-services/openai/quickstart?tabs=command-line&pivots=rest-api
 from twitchio.ext import commands
 import datetime
-import json
-import random
 import asyncio
+import random
+import json
 
 from src.config.libaray_config import get_twitch_access_token
 from src.chat_gpt import generate_chat_completion
 from src.text_to_speech import TextToSpeech
-from src.chat_filter import ChatFilter
-from src.rigging import check_rigging
-
-from xml.sax.saxutils import escape
-
+from src.config.prompt import get_mao_prompt3, get_mao_prompt
+from src.config.read_json import read_json_file
 
 import warnings
 warnings.filterwarnings('ignore')
 
-# 완전히 받아오는데 시간이 걸립니다.
-# 그래서 대략 10초 기다렸다가 테스트 진행해야 합니다.
 class Bot(commands.Bot):
     def __init__(self):
-        # super().__init__(token=twitch_access_token, prefix='', initial_channels=['kirisakirai'])
         super().__init__(token=get_twitch_access_token(), prefix='', initial_channels=['enqlqkr'])
-        self.messages = [{"role": "system", "content": "당신은 사람입니다. 연인과 대화하듯이 대화하세요."}]
+        # self.messages = [{"role": "system", "content": get_mao_prompt3()}]
+        self.messages = read_json_file('./mao.json')
         self.last_request_time = datetime.datetime.now()
         self.text_to_speech = TextToSpeech()
-        self.chat_filter = ChatFilter()
-        self.is_stt_active = False
+        self.is_speak = False
         print("성공적으로 받아왔습니다.")
 
     async def event_ready(self):
         print(f'Ready | {self.nick}')
 
     async def event_message(self, message):
-        if message.echo or self.is_stt_active:
+        if self.is_speak:
             return
-
-        user_content = message.content.lower()
-        filtered_user_content = self.chat_filter.filter_sensitive_words(user_content)
-
-        # 메시지를 컨텍스트에 추가
-        self.messages.append({"role": "user", "content": filtered_user_content})  
-
-        if self.is_too_long(filtered_user_content) or self.is_too_short(filtered_user_content):
-            print("GPT : 너무 짧아 무시됐습니다.")
-            return
-
-        # 혐오표현 필터링
-        if self.filter_bad_word(filtered_user_content):
-            await self.text_to_speech.speak("필터링", pitch='+15%', rate="+20%")
-            print("GPT: 필터링처리됐습니다.")
-            return
-
         
-        current_time = datetime.datetime.now()
-        time_diff = current_time - self.last_request_time
-        if time_diff.total_seconds() < random.randint(5, 10):
-            return
-
+        user_content = message.content.lower()
+        print(f"user : {user_content}")
+        # 메시지를 컨텍스트에 추가
+        # GPT 입력 형식을 맞추기 위한 용도
+        self.messages.append({"role": "user", "content": user_content})  
+        self.is_speak = True
+        
         # GPT 답변 생성
-        assistant_content = await generate_chat_completion(prompt=self.messages)
-
-        # 시간 최신화를 안 해주면 GPT가 임의로 대본을 생성해버린다.
-        self.last_request_time = datetime.datetime.now()
-        response_data = {"prompt": filtered_user_content, "completion": assistant_content}
-        self.write_chat_data(response_data)
-
+        assistant_content = await generate_chat_completion(prompt=self.messages, temperature=0.8)
+        
         print(f"GPT: {assistant_content}")
 
-        # 일부 특수문자 처리
-        escaped_content = escape(assistant_content)
-        tts_task = asyncio.create_task(self.text_to_speech.speak(escaped_content, pitch='+15%', rate="+20%"))
-        check_rigging(filtered_user_content)
+        # pitch : 음의 높낮이
+        # rate : 빠르기
+        tts_task = asyncio.create_task(self.text_to_speech.speak(assistant_content, pitch='+15%', rate="+20%"))
         await tts_task  # TTS 출력을 기다림
-
+        self.is_speak = False
         self.messages.append({"role": "assistant", "content": assistant_content})
-
-
-    def is_too_long(self, user_content):
-        return len(user_content) > 200
-    
-    def is_too_short(self, user_content):
-        return len(user_content) == 1
-    
-    def filter_bad_word(self, content):
-        return content == "필터링"
-
-    # 사용자 채팅 내역 저장
-    def write_chat_data(self, response_data):
-        with open('gpt_responses.jsonl', 'a', encoding='utf-8') as f:
-            f.write(json.dumps(response_data, ensure_ascii=False) + '\n')
