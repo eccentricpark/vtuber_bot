2300f5aab0cf12a5134dde224be7a4a954b9f66d
ParkJinho <pen40@naver.com>
2023-11-27 10:40:30 +0900

    마법 관련 동작 제어 추가 특정 키워드 처리는 띄어쓰기를 모두 제거하고 처리하는 방식으로 변경

--- bot.py
+++ bot.py
@@ -30,10 +30,10 @@ class Bot(commands.Bot):
     async def event_message(self, message):
         user_content = message.content
 
-        if user_content == "!발표모드":
-            self.is_announce = True
-        elif user_content == "!발표모드해제":
-            self.is_announce = False
+        # if user_content == "!발표모드":
+        #     self.is_announce = True
+        # elif user_content == "!발표모드해제":
+        #     self.is_announce = False
 
         # 토글에 따른 발표모드 제어
         if(self.is_announce):
@@ -59,7 +59,7 @@ class Bot(commands.Bot):
             assistant_content = "아직 이것 밖에 할 줄 모르지만, 이 곡이라도 연주해볼게."
             print(f"MAO : {assistant_content}")
 
-            await self.print_tts_sound(assistant_content)
+            await self.print_tts_sound(assistant_content, user_content)
             await act(system_message="play")
             play_sound(f"musics/달빛에그려지는.WAV")
         # 그 외 평범한 답변
@@ -67,7 +67,7 @@ class Bot(commands.Bot):
             # GPT 답변 생성
             assistant_content = get_ai_response(self.messages)
             print(f"MAO : {assistant_content}")
-            await self.print_tts_sound(assistant_content)
+            await self.print_tts_sound(assistant_content, user_content)
             await act(system_message="rollback")
 
         write_json(user_content, assistant_content)  
@@ -95,16 +95,25 @@ class Bot(commands.Bot):
 
     # 악기를 연주해달라는 내용이 있으면 바로 악기 연주
     def check_listen_instrument(self, user_content):
-        want_listen = ["악기 연주 해줘", "악기연주 해줘", "악기연주해줘", "악기연주 해 줘", "악기 연주 해 줘"]
-        # 조건에 따라 변수 값 변경
-        return user_content in want_listen
+        without_spaces = user_content.replace(" ", "")
+        keyword = "악기연주해줘"
+        return keyword in without_spaces
+    
+    # 마법을 보여달라고 하면 거기에 맞춰 동작
+    def check_magic(self, user_content):
+        without_spaces = user_content.replace(" ", "")
+        keywords = ["마법보여줘", "마법한번보여줘", "마술보여줘", "마술한번부려봐","마술한번해봐"]
+        return any(keyword in without_spaces for keyword in keywords)
             
     # TTS 음성 출력
-    async def print_tts_sound(self, assistant_content):
+    async def print_tts_sound(self, assistant_content, user_content=""):
         # pitch : 음의 높낮이
         # rate : 빠르기
         tts_task = asyncio.create_task(self.text_to_speech.speak(assistant_content, pitch='+15%', rate="+25%"))
-        await act(system_message="talk")
+        if(self.check_magic(user_content)):
+            await act(system_message="magic")
+        else:
+            await act(system_message="talk")
         await tts_task  # TTS 출력을 기다림
         self.is_speak = False
         
